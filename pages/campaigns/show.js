b37e828d26df8ef11ed11ab1c9a69ab4dc234e3b
Ivan S <ing.ivaneduardosanchez@hotmail.com>
2020-10-29 21:52:51 -0500

    Add get initial props to get the Campaign sumary

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
+import Campaign from "../../ethereum/campaign";
 
 class CampaignShow extends Component {
+  static getInitialProps = async (props) => {
+    const campaign = Campaign(props.query.address);
+    const sumary = await campaign.methods.getSummary().call();
+    console.log(sumary);
+    return {};
+  };
   render() {
     return (
       <Layout>
